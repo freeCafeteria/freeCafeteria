d100e5a3dbab1d2d21d2a674ea8ae39cbca85a86
kimhyunji <107786171+LOCAL-HJ@users.noreply.github.com>
2024-05-13 14:50:49 +0900

    Revert "design: 스플래시 화면 수정"

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import SplashScreen from "react-native-splash-screen";
 import Router from "./src/router";
 
 function App(): React.JSX.Element {
-  useEffect(() => {
-    setTimeout(() => {
-      SplashScreen.hide();
-    }, 1000); // 스플래시 활성화 시간
-  }, []);
-
   return (
     <NavigationContainer>
       <Router />
